ae0277604248de27781598211dd42b7462950d59
tralwdwd <79791517+tralwdwd@users.noreply.github.com>
2024-09-11 16:07:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ var btn = document.getElementById("pressbtn")
 btn.onclick = function (){
     document.location = "https://www.youtube.com/watch?v=4p64UKVdHYQ&ab_channel=Enderman";
 }
+
+function rickroll(){
+    document.location = "https://example.com"
+}
